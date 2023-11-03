79ec3cbfa151dd7c58abbd0d1e722ad787dfab6f
Camila Jaurena <jaurenacamila@gmail.com>
2023-11-03 12:39:26 -0300

    seed de servicios

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import indexRoutes from './routes/indexRoutes.js'
 import cors from 'cors'
+import seedServices from './seed/seedService.js';
 import { connectToDatabase } from './connection/connection.js'
 import { serverPort } from './config/config.js'
 
@@ -11,13 +12,23 @@ app.use(cors())
 app.use(indexRoutes);
 
 async function startServer() {
+
     try {
+
+        // Conecta con la base de datos
         await connectToDatabase();
+
+        // Ejecuta el seeding
+        await seedServices();
+
         app.listen(serverPort, () => {
             console.log('SERVER PORT:', serverPort);
         });
+
     } catch (err) {
+
         console.error('Error starting the server:', err);
+
     }
 }
   
