a7762cfc5cdd4a871f4fa24bc28ba233a60b65b9
JavierToso <ortjavier1995@gmail.com>
2023-11-23 18:53:30 -0300

    mas dummy data
    Se incorpora mas dummy data para la demo

--- backend/seed/seedUser.js
+++ backend/seed/seedUser.js
@@ -7,7 +7,7 @@ const userId2 = new mongoose.Types.ObjectId('5fc29be19f808d3a40c7c634');
 const userId3 = new mongoose.Types.ObjectId('5fc29be19f808d3a40c7c635');
 
 const userData = [
-    {_id: userId1, name: 'User1', surname: 'Test', email: 'test@test.com', password: 'abcd1234', location: 'Buenos Aires', phoneNumber: '1157552369', role: 'user', isEmailVerified: 'true'},
+    {_id: userId1, name: 'User1', surname: 'Test', email: 'test@test.com', password: 'abcd1234', location: 'Buenos Aires', phoneNumber: '1157552369', role: 'provider', isEmailVerified: 'true'},
     {_id: userId2, name: 'User2', surname: 'Admin', email: 'admin@admin.com', password: 'abcd1234', location: 'Buenos Aires', phoneNumber: '1157552369', role: 'user', isEmailVerified: 'true'},
     {_id: userId3, name: 'User3', surname: 'Dev', email: 'dev@dev.com', password: 'abcd1234', location: 'Buenos Aires', phoneNumber: '1157552369', role: 'user', isEmailVerified: 'true'},
 ];
